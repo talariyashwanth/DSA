77a262f0e62e71a8c1c9cd6c1863242443c50868
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-05 00:44:24 +0530

    test1commit

--- dsa_java/normal.java
+++
@@ -1,6 +0,0 @@
-public class normal {
-    public static void main(String[] args) {
-        String name = "Talari Yashwanth";
-        int age = 18;
-        System.out.println(name);
-    }
---
+++ dsa_java/test1.java
@@ -0,0 +1,8 @@
+public class test1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.println("a + b = " + c);
+    }
+}
