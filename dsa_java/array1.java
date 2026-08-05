a6f3d7e0a4dd6dcbaef7ac482b769731534b4aaf
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-05 08:53:44 +0530

    good start

---
+++ dsa_java/array1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.println("Enter a number: ");
+        for(int i = 0; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("You entered ");
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+}
