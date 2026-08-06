e153258724b209969195d734f4a065462ff5c167
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-06 10:54:14 +0530

    code done in university

---
+++ dsa_java/practice.java
@@ -0,0 +1,11 @@
+class sum{
+    void printvalue(){
+        for(int i = 1; i<11; i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+        sum obj = new sum();
+        obj.printvalue();
+    }
+}
