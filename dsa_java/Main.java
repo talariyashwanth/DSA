9a668f0ea521d9c901de5e4087deadbf9ddba141
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-06 11:04:05 +0530

    swapping numbers

---
+++ dsa_java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class swap{
+    int a,b;
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        a = sc.nextInt();
+        System.out.println("Enter b : ");
+        b = sc.nextInt();
+    }
+    void swap(){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    void display(){
+        System.out.println("After Swapping : ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        }
+    }
+    public class Main{
+        public static void main(String[] args) {
+            swap obj = new swap();
+            obj.input();
+            obj.swap();
+            obj.display();
+        }
+    }
+
