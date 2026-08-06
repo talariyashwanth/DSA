3fd987b744f57110e564708b5695854b5eb2ec9f
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-06 11:49:48 +0530

    divisibility check

---
+++ dsa_java/check.java
@@ -0,0 +1,14 @@
+/* Divisibility check by 3 and 5 */
+class Divisible {
+    void Calculation(){
+        for(int i = 0; i < 101; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("Numbers divisible by 3 and 5 are : " + i);
+            }
+        }
+    }
+    public static void main(String[] args){
+        Divisible obj = new Divisible();
+        obj.Calculation();
+    }
+}
