f7ef8d963c9b528641d5c5ca8423e223b61c035d
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-19 13:38:09 +0530

    feat: add basic JavaScript syntax examples and comments to index.js

--- JAVA_FSD_A1_2025281724/index.js
+++ JAVA_FSD_A1_2025281724/index.js
@@ -7,6 +7,7 @@
 // const c = 5;
 // console.log(c);
 
-var fname = "Talari";
-var lname = "Yashwanth";
-console.log(fname + " " + lname);
+// var fname = "Talari";
+// var lname = "Yashwanth";
+// console.log(fname + " " + lname);
+
