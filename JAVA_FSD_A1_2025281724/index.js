bd84ad5c6819376ba3f5be576966ebc99b28db00
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-19 14:15:05 +0530

    feat: add introductory JavaScript comments and array examples to index.js

--- JAVA_FSD_A1_2025281724/index.js
+++ JAVA_FSD_A1_2025281724/index.js
@@ -1,8 +1,10 @@
 //JS is a synchronous and single threaded programming language
 
-const arr = [1,2,3];
-const nums = [20, "Talari", null, true, undefined];
-nums[1] = "Yashwanth";
-console.log(arr);
-console.log(nums);
+// const arr = [1,2,3];
+// const nums = [20, "Talari", null, true, undefined];
+// nums[1] = "Yashwanth";
+// console.log(arr);
+// console.log(nums);
+
+// Objects
 
