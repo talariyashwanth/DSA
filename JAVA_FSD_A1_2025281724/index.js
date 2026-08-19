749547d2d13ea6409dc463047c300f22491edbee
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-19 14:13:33 +0530

    feat: initialize index.js with array operations and type examples

--- JAVA_FSD_A1_2025281724/index.js
+++ JAVA_FSD_A1_2025281724/index.js
@@ -1,23 +1,8 @@
 //JS is a synchronous and single threaded programming language
 
-// let a;
-// a = 20;
-// console.log(a);
-
-// const c = 5;
-// console.log(c);
-
-// var fname = "Talari";
-// var lname = "Yashwanth";
-// console.log(fname + " " + lname);
-
-console.log(b);
-var b = 60;
-let a = 20;
-var b;
-b = 50;
-a = 30;
-console.log(a);
-const c = 70;
-console.log(c);
+const arr = [1,2,3];
+const nums = [20, "Talari", null, true, undefined];
+nums[1] = "Yashwanth";
+console.log(arr);
+console.log(nums);
 
