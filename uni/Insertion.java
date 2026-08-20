439c5bf775ce8ad9c03aa7669f2c838f4cf3ba71
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-20 11:17:23 +0530

    feat: add implementation of insertion sort algorithm

--- uni/Insertion.java
+++ uni/Insertion.java
@@ -5,7 +5,11 @@ public class Insertion{
         for(int i = 1; i < len; i++){
             int key = arr[i];
             int j = i-1;
-            while(j >= 0; )
+            while(j >= 0 && key < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
         }
     }
 }
