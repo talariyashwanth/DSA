6a2ad07f9203c193b6f9a1c4b31385da14d75cbe
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-20 11:13:07 +0530

    feat: add initial implementation of insertion sort algorithm

--- uni/bubblesort.java
+++
@@ -1,6 +0,0 @@
-class bubblesort{
-    public static void main(String[] args){
-        int[] arr = {6,3,8,5,2};
-        
-    }
-}
---
+++ uni/insertionsort.java
@@ -0,0 +1,10 @@
+class insertionsort{
+    public static void main(String[] args){
+        int[] arr = {6,3,8,5,2};
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i+1] < i){
+                
+            }
+        }
+    }
+}
