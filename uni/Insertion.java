1d027e2e4c075ebae5d84b42b5fb632b2dbcfba9
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-20 11:37:08 +0530

    feat: implement insertion sort algorithm in Insertion.java

--- uni/Insertion.java
+++ uni/Insertion.java
@@ -1,16 +1,15 @@
 import java.util.*;
 public class Insertion{
-    public static void main(String[] args){
-        int len = arr.length;
-        for(int i = 1; i < len; i++){
-            int key = arr[i];
-            int j = i-1;
-            while(j >= 0 && key < arr[j]){
-                arr[j+1] = arr[j];
-                j--;
-            }
-            arr[j+1] = key;
+    int len = arr.length;
+    for(int i = 1; i < len; i++){
+        int key = arr[i];
+        int j = i-1;
+        while(j >= 0 && key < arr[j]){
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = key;
+
     public static void main(String[] args){
         Insetion Obj = new Insertion();
         Scanner sc = new Scanner(System.in);
@@ -24,7 +23,7 @@ public class Insertion{
         System.out.println("After sorting, the elements in array: ");
         Obj.insertionSort(arr);
         for(int i = 0; i < n; i++){
-            
+            System.out.println(arr[i]);
         }
     }
     }
