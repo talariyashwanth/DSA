fa596146835f87fb218d90dedae94dae154f9bf9
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-25 14:21:58 +0530

    feat: add test class with nested loop logic and helper method

---
+++ Patterns/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void sms(int a, int b) {
+        System.out.print(a+b);
+    }
+    public static void main(String[] args) {
+        int c = 2;
+        for(int a = 1; a <= 5; a++){
+            for(int b = 1; b <= a; b++){
+                if(a == c && b == c){
+                    sms(a,c);
+                }
+                System.out.println
+            }
+        }
+    }
+}
