3c21ea9df0ac17bff352601d7a8891fd1a6f8430
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-25 14:05:03 +0530

    feat: add tea ingredient calculation logic in tea.java

--- Patterns/tea.java
+++ Patterns/tea.java
@@ -3,6 +3,17 @@ public class tea {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter quantity for TEA");
-        int req = s.nextInt();
+        int req = sc.nextInt();
+        float avl_M = 20.0f;
+        float avl_S = 20.0f;
+        float avl_TL = 20.0f;
+
+        float cons_M = 0.5f;
+        float cons_S = 0.3f;
+        float cons_TL = 0.2f;
+
+        float req_of_M = req*cons_M;
+        float req_of_S = req*cons_S;
+        float rel;
     }
 }
