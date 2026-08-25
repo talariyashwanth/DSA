de96c27f51c3a84b3a63723567d1b15c65af11d7
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-08-25 14:02:59 +0530

    feat: add tea quantity input implementation in tea.java

---
+++ Patterns/tea.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class tea {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter quantity for TEA");
+        int req = s.nextInt();
+    }
+}
