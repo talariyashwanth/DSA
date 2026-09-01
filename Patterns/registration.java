575750c5e0eeac4dd514631ecfc24f16e29b910b
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-09-01 14:03:19 +0530

    feat: add User class with name and password fields to registration.java

--- Patterns/registration.java
+++ Patterns/registration.java
@@ -1,5 +1,14 @@
-class Registration{
+import java.util.Scanner;
+class User{
     private String Name;
     private int Password;
     
+    public User(String Name, int Password){
+        this.Name = Name;
+        this.Password = Password;
+    }
+
+    public String getName(){
+        return Name;
+    }
 }
