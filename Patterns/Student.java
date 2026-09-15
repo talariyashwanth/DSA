40b5c0691232c974af70e69440795150311eea6e
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-09-15 13:56:00 +0530

    feat: add Student class with default and parameterized constructors

--- Patterns/Student.java
+++ Patterns/Student.java
@@ -7,5 +7,13 @@ class Student {
         roll = 0;
     }
 
-    Student(String name, int roll)
+    Student(String name, int roll){
+        this.name = name;
+        this.roll = roll;
+    }
+
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Roll : " + roll);
+    }
 }
