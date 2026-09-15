8922f7a43ffdf13de7cff353674c573486984600
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-09-15 13:54:06 +0530

    feat: add Student class with default and parameterized constructors

--- Patterns/Student.java
+++ Patterns/Student.java
@@ -1,3 +1,12 @@
 class Student {
-    
+    String name;
+    int roll;
+
+    Student() {
+        name = "Unknown";
+        age = 0;
+        branch = "Not Assigned";
+    }
+
+    Student(String name, int roll)
 }
