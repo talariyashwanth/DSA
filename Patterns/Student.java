7c2144b52f266276d9ef84a908ca61a60a7fb800
Yashwanth Talari <213832362+talariyashwanth@users.noreply.github.com>
2026-09-15 13:59:24 +0530

    feat: add Student class with constructors and main execution method

--- Patterns/Student.java
+++ Patterns/Student.java
@@ -15,5 +15,15 @@ class Student {
     void display() {
         System.out.println("Name : " + name);
         System.out.println("Roll : " + roll);
+        System.out.println();
+    }
+}
+
+public class student{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Yashwanth", 2025281724);
+        s1.display();
+        s2.display();
     }
 }
