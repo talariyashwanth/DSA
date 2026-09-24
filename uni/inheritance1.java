15f270ca938a93796227ee4374e9a25335890274
Yashwanth Talari <ft.yashwanth.talari@gmail.com>
2026-09-24 11:02:23 +0530

    another commit

--- uni/inheritance1.java
+++ uni/inheritance1.java
@@ -3,16 +3,17 @@ public class inheritance1 {
     public static String removeDuplicates(String s){
         int index = 0;
         char[] charArray = s.toCharArray();
-        int j;
-        for(int i  = 0; j<i; i++){
-            if(s.charAt(i) == s.charAt(j))
-                break;
-        }
-        if( j== i){
-            charArray[index++] = s.charAt(i);
+        for(int i = 0; i < s.length(); i++){
+            int j;
+            for(j = 0; j < i; j++){
+                if(s.charAt(i) == s.charAt(j))
+                    break;
+            }
+            if(j == i)
+                charArray[index++] = s.charAt(i);
         }
         return new String(charArray, 0, index);
-        }
+    }
     public static void main(String[] args) {
         
     }
